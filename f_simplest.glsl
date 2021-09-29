549a9e6ae4cda16ebf2b280f1c75e1c6cd9fa2c8
Dominik Sawicki <dominik.sawicki52@gmail.com>
2021-09-29 11:22:16 +0200

    Final changes

--- f_simplest.glsl
+++ f_simplest.glsl
@@ -9,22 +9,30 @@ out vec4 pixelColor; //Zmienna wyjsciowa fragment shadera. Zapisuje sie do niej 
 
 in vec4 ic; 
 in vec4 n;
+in vec4 n2;
 in vec4 l;
+in vec4 l2;
 in vec4 v;
+in vec4 v2;
 in vec2 iTexCoord0;
 
 void main(void) {
 
 	//Znormalizowane interpolowane wektory
 	vec4 ml = normalize(l);
+	vec4 ml2 = normalize(l2);
 	vec4 mn = normalize(n);
+	vec4 mn2 = normalize(n2);
 	vec4 mv = normalize(v);
+	vec4 mv2 = normalize(v2);
 	//Wektor odbity
 	vec4 mr = reflect(-ml, mn);
+	vec4 mr2 = reflect(-ml2, mn2);
 
 	//Parametry powierzchni
 	vec4 kd = texture(textureMap0, iTexCoord0);
 	vec4 ks = vec4(1,1,1,1);
+	vec4 ks2 = vec4(1,1,1,1);
 
 	// Uwzglednienie tekstury odbic (jezeli zostala podana)
 	if (specular == 1){
@@ -33,8 +41,10 @@ void main(void) {
 
 	//Obliczenie modelu oświetlenia
 	float nl = clamp(dot(mn, ml), 0, 1);
+	float nl2 = clamp(dot(mn, ml2), 0, 1);
 	float rv = pow(clamp(dot(mr, mv), 0, 1), 20);
-	pixelColor = vec4(kd.rgb * nl, kd.a) + vec4(ks.rgb*rv, 0);
+	float rv2 = pow(clamp(dot(mr2, mv2), 0, 1), 20);
+	pixelColor = vec4(kd.rgb * nl, kd.a) + vec4(ks.rgb*rv, 0) + vec4(kd.rgb * nl2, kd.a) + vec4(ks2.rgb*rv2, 0);
 
 	//Podswietlanie wybranego obiektu
 	if (chosen == 1){
--- v_simplest.glsl
+++ v_simplest.glsl
@@ -14,15 +14,22 @@ in vec2 texCoord0;
 //Zmienne interpolowane
 out vec4 ic;
 out vec4 l;
+out vec4 l2;
 out vec4 n;
+out vec4 n2;
 out vec4 v;
+out vec4 v2;
 out vec2 iTexCoord0;
 
 void main(void) {
     vec4 lp = vec4(0, 5, 0, 1); //pozcyja światła, przestrzeń świata
+    vec4 lp2 = vec4(5, 5, 5, 1);
     l = normalize(V * lp - V*M*vertex); //wektor do światła w przestrzeni oka
+    l2 = normalize(V * lp2 - V*M*vertex);
     v = normalize(vec4(0, 0, 0, 1) - V * M * vertex); //wektor do obserwatora w przestrzeni oka
+    v2 = normalize(vec4(0, 0, 0, 1) - V * M * vertex); //wektor do obserwatora w przestrzeni oka
     n = normalize(V * M * normal); //wektor normalny w przestrzeni oka
+    n2 = normalize(V * M * normal); //wektor normalny w przestrzeni oka
     iTexCoord0 = texCoord0; 
     ic = color;
 
